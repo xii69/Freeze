745625fbd28c00b19e59f6ccc4b4fd09665b40e0
xii69 <xii69@yahoo.com>
2023-01-21 22:45:34 +0330

    Uploaded project base files

---
+++ src/main/java/me/xii69/freeze/Main.java
@@ -0,0 +1,17 @@
+package me.xii69.freeze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
